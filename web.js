e370c74ce20124fbf4b1b680aea0f7f8ba29b8de
Pedro Lopez <plp750@gmail.com>
2013-07-09 23:05:21 +0200

    debug info

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', 'utf8', function(err, data) {
 	if(err) throw err;
-	//response.send(data);
 	console.log(data);
+	response.send(data);
   });
   //response.send('Hello World 2!');
 });
