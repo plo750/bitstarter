9d3043767f0a02ee7455b131f1d740c2fe723828
Pedro Lopez <plp750@gmail.com>
2013-07-09 22:37:23 +0200

    Read data from file

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function(err, data) {
+	if(err) throw err;
+	response.send(data);
+  });
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
